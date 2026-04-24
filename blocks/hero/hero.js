2e10412eccbb1b1e7183ac34a7438defb53f6fd1
sapna <sapna.s@techchefz.com>
2026-04-24 18:50:29 +0530

    merge code

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,8 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const picture = block.querySelector('picture');
+  const pictures = [...block.querySelectorAll('picture')];
+  const [desktopPicture, mobilePicture] = pictures;
   const heading = block.querySelector('h1, h2, h3, h4, h5, h6');
   const paragraphs = [...block.querySelectorAll('p')].filter((p) => !p.querySelector('picture'));
 
@@ -17,6 +18,15 @@ export default function decorate(block) {
   paragraphs.forEach((p) => textContainer.appendChild(p));
 
   block.textContent = '';
-  if (picture) block.appendChild(picture);
+
+  if (desktopPicture) {
+    desktopPicture.classList.add('hero-picture-desktop');
+    block.appendChild(desktopPicture);
+  }
+  if (mobilePicture) {
+    mobilePicture.classList.add('hero-picture-mobile');
+    block.appendChild(mobilePicture);
+  }
+
   block.appendChild(textContainer);
 }
